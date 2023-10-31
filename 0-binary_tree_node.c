0e0a03d8323469a454547980bca461d82370117a
John Gaitho <johngaitho05@gmail.com>
2023-10-31 21:20:41 +0300

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,12 +8,14 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
 	if (!node)
 		return (NULL);
 	node->parent = parent;
 	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
 
 	return (node);
 }
