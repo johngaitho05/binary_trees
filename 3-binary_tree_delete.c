77b93e92422c06c80a4f47840a4a558831140306
John Gaitho <johngaitho05@gmail.com>
2023-10-31 22:05:16 +0300

    3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -4,7 +4,7 @@
  * binary_tree_delete - deletes an entire binary tree
  * @tree: pointer to root node
  */
-void binary_tree_delete(binary_tree_t* tree)
+void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
 	{
