db503d09986c85a533e3146bce15e2aceaa870df
John Gaitho <johngaitho05@gmail.com>
2023-11-01 20:35:42 +0300

    102-binary_tree_is_complete.c

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,59 +1,45 @@
 #include "binary_trees.h"
 
 /**
- * max - compares 2 numbers to find the greater one
- * @x: first number
- * @y: second number
- * Return: the greater number
- */
-int max(int x, int y)
-{
-	return ((x > y) ? x : y);
-}
-
-/**
- * binary_tree_height - computes the height of a binary tree
- * @tree: the tree to compute height for
- * Return: the height of the tree
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	if (!tree || !(tree->left || tree->right))
-		return (0);
-	return (max(binary_tree_height(tree->left),
-				binary_tree_height(tree->right)) + 1);
-}
-
-
-
-/**
  * binary_tree_is_complete - Checks if a binary tree is complete.
  * @tree: Pointer to the root of the binary tree.
  * Return: 1 if the tree is complete, 0 if it is not.
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	size_t  left_height, right_height;
+
+	/* Flag to indicate if a NULL node is encountered */
+	bool null_node_found = false;
+
+	/* Queue for level-order traversal */
+	const binary_tree_t *queue[10000];
+	int front = -1, rear = -1;
 
 	if (!tree)
-		return (0);
+		return (1);
 
-	if (!tree->left && tree->right)
-		return (0);
+	/* Enqueue the root node */
+	queue[++rear] = tree;
 
-	if (tree->left && !binary_tree_is_complete(tree->left))
-		return (0);
+	while (front != rear)
+	{
+		const binary_tree_t *current = queue[++front];
 
-	if (tree->right && !binary_tree_is_complete(tree->right))
-		return (0);
+		if (current == NULL)
+		{
+			null_node_found = true;
+		}
+		else
+		{
+			/* If a NULL node was previously encountered, the tree is not complete */
+			if (null_node_found)
+				return (0);
 
-	if (tree->left && tree->right && tree->right->left && !tree->left->right)
-		return (0);
-
-    left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	if (!(left_height == right_height || left_height == right_height + 1))
-		return (0);
+			/* Enqueue the left and right children of the current node */
+			queue[++rear] = current->left;
+			queue[++rear] = current->right;
+		}
+	}
 
 	return (1);
 }
