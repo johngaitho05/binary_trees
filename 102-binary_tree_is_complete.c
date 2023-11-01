808c35796078d68200de6a16f218e15a7270a94a
John Gaitho <johngaitho05@gmail.com>
2023-11-01 20:09:34 +0300

    102-binary_tree_is_complete.c

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,5 +1,30 @@
 #include "binary_trees.h"
 
+/**
+ * max - compares 2 numbers to find the greater one
+ * @x: first number
+ * @y: second number
+ * Return: the greater number
+ */
+int max(int x, int y)
+{
+	return ((x > y) ? x : y);
+}
+
+/**
+ * binary_tree_height - computes the height of a binary tree
+ * @tree: the tree to compute height for
+ * Return: the height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree || !(tree->left || tree->right))
+		return (0);
+	return (max(binary_tree_height(tree->left),
+				binary_tree_height(tree->right)) + 1);
+}
+
+
 
 /**
  * binary_tree_is_complete - Checks if a binary tree is complete.
@@ -21,6 +46,9 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left && tree->right && tree->right->left && !tree->left->right)
+		return (0);
+
+	if (binary_tree_height(tree->left) > binary_tree_height(tree->right) + 1)
 		return (0);
 
 	return (1);
