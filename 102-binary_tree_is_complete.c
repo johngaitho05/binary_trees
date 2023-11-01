34f35af06b6d245a416528eda9225d5c53476c97
John Gaitho <johngaitho05@gmail.com>
2023-11-01 19:57:40 +0300

    102-binary_tree_is_complete.c

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 
+
 /**
  * binary_tree_is_complete - Checks if a binary tree is complete.
  * @tree: Pointer to the root of the binary tree.
@@ -9,16 +10,18 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if (!tree->left && !tree->right)
-		return (1);
-	if (tree->right && !tree->left)
+
+	if (!tree->left && tree->right)
 		return (0);
-	if (tree->left && tree->right)
-	{
-		if ((tree->right->left || tree->right->right)  && !tree->left->right)
-			return (0);
-		return (binary_tree_is_complete(tree->left) &&
-				binary_tree_is_complete(tree->right));
-	}
-	return (binary_tree_is_complete(tree->left));
+
+	if (tree->left && !binary_tree_is_complete(tree->left))
+		return (0);
+
+	if (tree->right && !binary_tree_is_complete(tree->right))
+		return (0);
+
+	if (tree->left && tree->right && tree->right->left && !tree->left->right)
+		return (0);
+
+	return (1);
 }
