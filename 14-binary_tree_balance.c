9922559bfee84a662577ab80c5f450167a21fff3
John Gaitho <johngaitho05@gmail.com>
2023-11-01 07:55:14 +0300

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,18 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * max - compares 2 numbers to find the greater one
+ * _max - compares 2 numbers to find the greater one
  * @x: first number
  * @y: second number
  * Return: the greater number
  */
-int max(int x, int y)
+int _max(int x, int y)
 {
 	return ((x > y) ? x : y);
 }
 
 /**
- * binary_tree_height - computes the height of a binary tree
+ * _binary_tree_height - computes the height of a binary tree
  * @tree: the tree to compute height for
  * Return: the height of the tree
  */
@@ -22,7 +22,7 @@ size_t _binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	if (!(tree->left || tree->right))
 		return (1);
-	return (max(_binary_tree_height(tree->left),
+	return (_max(_binary_tree_height(tree->left),
 				_binary_tree_height(tree->right)) + 1);
 }
 
