4a695ca993e7472084c2dff587f835f472f72899
John Gaitho <johngaitho05@gmail.com>
2023-11-02 22:50:42 +0300

    130-binary_tree_is_heap.c

--- binary_trees.h
+++ binary_trees.h
@@ -64,5 +64,6 @@ avl_t *array_to_avl(int *array, size_t size);
 avl_t *balance_avl_tree(avl_t *tree);
 avl_t *avl_remove(avl_t *root, int value);
 avl_t *sorted_array_to_avl(int *array, size_t size);
+int binary_tree_is_heap(const binary_tree_t *tree);
 
 #endif
