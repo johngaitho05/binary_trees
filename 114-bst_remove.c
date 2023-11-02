dd9c75ac2ef1fcc12c7263f5ec3b3c064293560b
John Gaitho <johngaitho05@gmail.com>
2023-11-02 08:44:58 +0300

    120-binary_tree_is_avl.c

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -24,8 +24,8 @@ bst_t *bst_remove(bst_t *root, int value)
 {
 	bst_t *temp;
 
-	if (root == NULL)
-		return (root);
+	if (!root)
+		return (NULL);
 
 	if (value < root->n)
 		root->left = bst_remove(root->left, value);
