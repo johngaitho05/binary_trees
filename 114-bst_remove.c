ff42e190ba502734a722bb05cf76bc7a5166eb18
John Gaitho <johngaitho05@gmail.com>
2023-11-02 09:52:43 +0300

    114-bst_remove.c

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,107 +1,112 @@
 #include "binary_trees.h"
 
 /**
- * swap - swaps two nodes in binary tree
- * @node: first node
- * @new: second node
- * Return: pointer to root
- */
-bst_t *swap(bst_t *node, bst_t *new)
-{
-	bst_t *tmp = NULL;
-	_Bool left_child = false;
-
-	if (node->parent)
-		left_child = node->parent->left == node;
-	if (new->parent && new->parent != node)
-		new->parent->left = NULL;
-	new->parent = node->parent;
-	if (node->parent)
-	{
-		if (left_child)
-			node->parent->left = new;
-		else
-			node->parent->right = new;
-	}
-	if (node->left != new)
-	{
-		new->left = node->left;
-		node->left->parent = new;
-	}
-	if (node->right && node->right != new)
-	{
-		new->right = node->right;
-		node->right->parent = new;
-	}
-	tmp = new;
-	while (tmp->parent)
-		tmp = tmp->parent;
-	free(node);
-	return (tmp);
-}
-
-/**
- * bst_remove - removes a node from a Binary Search Tree
- * @root: pointer to root of tree
- * @value: input value
- * Return: pointer to the new root node of the tree after removing the
- * desired value
- */
-bst_t *bst_remove(bst_t *root, int value)
-{
-	bst_t *node, *tmp;
-	_Bool left_child = false;
-
-	if (!root)
-		return (NULL);
-	node = bst_search(root, value);
-	if (!node)
-		return (NULL);
-	if (node->parent)
-		left_child = node->parent->left == node;
-	if (!node->right && !node->left)
-	{
-		if (!node->parent)
-		{
-			free(node);
-			return (NULL);
-		}
-		if (left_child)
-			node->parent->left = NULL;
-		else
-			node->parent->right = NULL;
-		tmp = node->parent;
-		while (tmp->parent)
-			tmp = tmp->parent;
-		free(node);
-		return (tmp);
-
-	}
-	if (!node->right)
-		return (swap(node, node->left));
-	tmp = node->right;
-	while (tmp->left)
-		tmp = tmp->left;
-	return (swap(node, tmp));
-}
-
-
-/**
- * bst_search - searches for a value in a Binary Search Tree
- * @tree: pointer to root of tree
- * @value: input value
- * Return: pointer to the node containing a value equals to value
+ * bst_search - search for value in binary search tree
+ * @tree: root of tree to search
+ * @value: value to search for
+ *
+ * Return: pointer to node containing value, or NULL if not found
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
-	if (!tree)
+	if (tree == NULL)
 		return (NULL);
-	while (tree)
+	if (value == tree->n)
+		return ((bst_t *) tree);
+	else if (value < tree->n)
+		return (bst_search(tree->left, value));
+	else
+		return (bst_search(tree->right, value));
+}
+
+/**
+ * simple_remove - subroutine to remove node of BST when it has no children
+ * @root: root of tree in which `ophan` resides
+ * @ophan: node to remove
+ * @parent: pointer to member of parent that points to `ophan`
+ *
+ * Return: pointer to root in case root is replaced
+ */
+bst_t *simple_remove(bst_t *root, bst_t *ophan, bst_t **parent)
+{
+	if (ophan->parent == NULL)
 	{
-		if (value == tree->n)
-			return ((bst_t *)tree);
-		tree = value < tree->n ? tree->left
-							   : tree->right;
+		free(ophan);
+		return (NULL);
 	}
-	return ((bst_t *)tree);
+	*parent = NULL;
+	free(ophan);
+	return (root);
+}
+
+/**
+ * left_remove - subroutine to remove node of BST for an only left child
+ * @root: root of tree in which `ophan` resides
+ * @ophan: node to remove
+ * @parent: pointer to member of parent that points to `ophan`
+ *
+ * Return: pointer to root in case root is replaced
+ */
+bst_t *left_remove(bst_t *root, bst_t *ophan, bst_t **parent)
+{
+	ophan->left->parent = ophan->parent;
+	if (ophan->parent == NULL)
+	{
+		root = ophan->left;
+		free(ophan);
+		return (root);
+	}
+	*parent = ophan->left;
+	free(ophan);
+	return (root);
+}
+
+/**
+ * bst_remove - remove node from binary search tree with value equal to `value`
+ * @root: root of tree from which to remove node
+ * @value: value of node to remove
+ *
+ * Return: pointer to new root of tree
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+	bst_t **parent, *node, *ophan = bst_search(root, value);
+
+	if (ophan == NULL)
+		return (root);
+	if (ophan->parent != NULL)
+		parent = (ophan == ophan->parent->left ?
+				  &ophan->parent->left : &ophan->parent->right);
+	if (ophan->right == NULL)
+	{
+		if (ophan->left == NULL)
+			return (simple_remove(root, ophan, parent));
+		return (left_remove(root, ophan, parent));
+	}
+	else
+	{
+		node =  ophan->right;
+		while (node->left != NULL)
+			node =  node->left;
+		if (node != ophan->right)
+		{
+			ophan->right->parent = node;
+			node->parent->left = node->right;
+			if (node->right != NULL)
+				node->right->parent = node->parent;
+			node->right = ophan->right;
+		}
+		if (ophan->left != NULL)
+			ophan->left->parent = node;
+		node->left = ophan->left;
+		node->parent = ophan->parent;
+		if (ophan->parent == NULL)
+		{
+			free(ophan);
+			return (node);
+		}
+		free(ophan);
+		*parent = node;
+		return (root);
+	}
 }
