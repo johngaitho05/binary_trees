fae57e654267b2a47dbbc3d04c856d3912281010
John Gaitho <johngaitho05@gmail.com>
2023-11-02 14:43:05 +0300

    123-avl_remove.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,49 @@
 #include "binary_trees.h"
 
 /**
+ * avl_balance_tree - balances an AVL tree
+ * @tree: pointer to root node
+ * Return: root node of the balanced tree
+ */
+avl_t *avl_balance_tree(avl_t *tree)
+{
+	int balance;
+
+	if (!tree)
+		return (NULL);
+
+	/* Recursively balance the left and right subtrees */
+	tree->left = avl_balance_tree(tree->left);
+	tree->right = avl_balance_tree(tree->right);
+
+	/* Check the balance factor of the current node */
+	balance = binary_tree_balance(tree);
+
+	/* Perform rotations if needed */
+	if (balance > 1 && binary_tree_balance(tree->left) >= 0)
+	{
+		return (binary_tree_rotate_right(tree));
+	}
+	if (balance < -1 && binary_tree_balance(tree->right) <= 0)
+	{
+		return (binary_tree_rotate_left(tree));
+	}
+	if (balance > 1 && binary_tree_balance(tree->left) < 0)
+	{
+		tree->left = binary_tree_rotate_left(tree->left);
+		return (binary_tree_rotate_right(tree));
+	}
+	if (balance < -1 && binary_tree_balance(tree->right) > 0)
+	{
+		tree->right = binary_tree_rotate_right(tree->right);
+		return (binary_tree_rotate_left(tree));
+	}
+
+	return (tree);
+}
+
+
+/**
  * _max - compares 2 numbers to find the greater one
  * @x: first number
  * @y: second number
@@ -39,3 +82,4 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (_binary_tree_height(tree->left) - _binary_tree_height(tree->right));
 }
+
